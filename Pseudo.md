b619b06bf2eaabfb84efca11663e54169e3cdb9b
hmgreen44 <68399145+hmgreen44@users.noreply.github.com>
2021-03-05 09:41:14 -0500

    working js

---
+++ Pseudo.md
@@ -0,0 +1,18 @@
+As an uninformed user I need to display the weather by zip code.
+
+Program Flow
+
+global - set default vars and const
+
+init- initialize the state, handlers, and render the view on load of the body
+
+render - display the info and data stored in state
+
+fetchWeatherData - pass in a zip code and return JSOn data to the state 
+
+handler - onload, click
+
+state - zip code, weather date, 
+
+input - user supply the zip code (get the user's geolocation and use it instead of location)
+
---
+++ main.js
@@ -0,0 +1,44 @@
+//Declaring variables and constants
+const zipInput = document.getElementById("zipInput");
+const weatherButton = document.getElementById("weatherButton");
+const cityOutput = document.getElementById("cityOutput");
+const outputK = document.getElementById("outputK");
+const outputF = document.getElementById("outputF");
+const outputC = document.getElementById("outputC");
+const condOutput = document.getElementById("condOutput");
+const infoOutput = document.getElementById("infoOutput");
+
+
+//async function
+function getWeather() {
+    const API_key = '8552952194f03a7096ea7450676ab6c3';
+    //getting user zipcodevalue
+    let zipCode = zipInput.value
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${API_key}`; 
+                    //https://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
+    console.log(zipCode);
+    console.log(apiUrl);
+
+    fetch(apiUrl)
+        .then(function (response) {
+            return response.json()
+
+        })
+        .then(function (json) {
+            console.log(json);
+            cityOutput.innerHTML = json.name;
+
+            outputK.innerHTML = Math.round(json.main.temp);
+
+            outputF.innerHTML = Math.round(((json.main.temp - 273.15) * 9 / 5) + 32)
+
+            outputC.innerHTML = Math.round(json.main.temp - 273.15);
+
+            let conditionOutput = json.weather[0].description;
+            condOutput.innerHTML = conditionOutput;
+           
+            infoOutput.src = `http://openweathermap.org/img/wn/${json.weather[0].icon}@4x.png`;
+        })
+}
+weatherButton.addEventListener('click', getWeather);
+
